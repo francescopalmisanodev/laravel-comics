5e1eed9cb908d042eecacef9b3296daa3b9efff3
francescopalmisanodev <francescopalmisano.dev@gmail.com>
2022-01-19 19:06:36 +0100

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <div class="container">
+        <a href="{{ route('home') }}">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="DC COMICS">
+        </a>
+        <nav>
+            <ul>
+                <li>Comics</li>
+                <li>News</li>
+            </ul>
+        </nav>
+    </div>
+</header>
